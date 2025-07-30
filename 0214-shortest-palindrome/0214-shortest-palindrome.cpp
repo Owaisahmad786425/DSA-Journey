c811a0c191963933bf71ead3300252d901028d2a
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-07-30 23:34:08 +0530

    Time: 3 ms (71.42%), Space: 18.2 MB (5.98%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,15 +1,32 @@
 class Solution {
+    vector<int> calculate_lps(vector<int> &lps,string res){
+        lps[0]=0;
+       int len = 0;
+        int i = 1;
+        while (i < res.size()) {
+            if (res[i] == res[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
 public:
     string shortestPalindrome(string s) {
-       string rev=s;
-       reverse(rev.begin(),rev.end());
-       int n=s.size();
-       for(int i=0;i<n;i++){
-          if(!memcmp(s.c_str(),rev.c_str()+i,n-i)){
-            return rev.substr(0,i)+s;
-          }
-       }
-return rev+s;
-
-     }
+             string rev=s;
+             int n=s.size();
+        reverse(rev.begin(),rev.end());
+        string res=s+'-'+rev;
+        vector<int> lps(res.size());
+        lps=calculate_lps(lps,res);
+        return rev.substr(0,n-lps[res.size()-1])+s;
+    }
 };
