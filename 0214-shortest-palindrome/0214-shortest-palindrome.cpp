30ffb939409ab6c342d5ff4a0c73340bb7a2ae67
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-07-30 23:07:49 +0530

    Time: 16 ms (7.38%), Space: 12.3 MB (86.04%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+       string rev=s;
+       reverse(rev.begin(),rev.end());
+       int n=s.size();
+       for(int i=0;i<n;i++){
+          if(memcmp(s.c_str(),rev.c_str()+i,n-i)==0){
+            return rev.substr(0,i)+s;
+          }
+       }
+return rev+s;
+
+     }
+};
