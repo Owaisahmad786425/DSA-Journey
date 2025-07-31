2b423b4e95a47eb49f6758c8b91477acc8a9c61b
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-07-31 23:40:18 +0530

    Time: 0 ms (100%), Space: 8 MB (64.48%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,33 +1,23 @@
 class Solution {
-    bool static comp(pair<char,int> p1,pair<char,int> p2){
-        return p1.first>p2.first;
-    }
 public:
     int maximumSwap(int num) {
         string s=to_string(num);
         int n=s.size();
-        vector<pair<char,int>> vec(s.size());
-        int i=0;
-        for(auto &it:s){
-            vec.push_back({it,i});
-            i++;
+        vector<int> digits(10);
+        for(int i=0;i<n;i++){
+           digits[s[i]-'0']=i;
         }
-        sort(vec.begin(),vec.end(),comp);
-        int pointer=0;
-        int j=0;
-        while(pointer<n){
-            if(s[pointer]!=vec[j].first && vec[j].first!=vec[j+1].first){
-              swap(s[pointer],s[vec[j].second]);
-              break;
+        for(int i=0;i<n;i++){
+            bool flag=0;
+            for(int j=9;j>=0;j--){
+                if(s[i]<j+'0' && digits[j]>i){
+                    swap(s[i],s[digits[j]]);
+                    flag=true;
+                    break;
+                }
             }
-            else if(s[pointer]!=vec[j].first && vec[j].first==vec[j+1].first){
-                j++;
-            }
-            else{
-            pointer++;
-            j++;
-            }
+            if(flag) break;
         }
-return stoi(s);
+        return stoi(s);
     }
 };
