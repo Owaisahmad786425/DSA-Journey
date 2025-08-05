2d436f20bd471d30a3e1b7b2ed242a86915280c3
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-05 22:06:14 +0530

    Time: 7 ms (89.15%), Space: 32 MB (88.09%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> freq(26,0);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+        }
+        int res=0;
+        for(int i=0;i<26;i++){
+        if(freq[i]==0) continue;
+         if(freq[i]%2==0) res+=2;
+         else{
+            res+=1;
+         } 
+        }
+        return res;
+    }
+};
