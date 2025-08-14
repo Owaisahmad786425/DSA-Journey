ead1d42cf8404c309355a5165dcc55d7bc0bd180
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-15 00:27:23 +0530

    Time: 0 ms (100%), Space: 9.6 MB (64.46%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,34 +3,22 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
         res.push_back({1});
-  for(int k=1;k<numRows;k++){
-
-int i=-1;
-int j=0;
-vector<int> temp;
-int n=res[k-1].size();
-while(i<n){
-    if(i==-1){
-        temp.push_back(res[k-1][j]);
-        i++;
-        j++;
-
-    }
-    else if(j==n){
-        temp.push_back(res[k-1][i]);
-        i++;
-        j++;
-    }
-    else {
-        int sum=res[k-1][i]+res[k-1][j];
-        temp.push_back(sum);
-        i++;
-        j++;
-    }
-}
-res.push_back(temp);
-  }
-return res;
-
+        for(int k=1;k<numRows;k++){
+            vector<int> temp(res.back().size()+1);
+            vector<int> store;
+            store=res.back();
+            temp[0]=1;
+            temp.back()=1;
+            int i=0,j=1;
+            int idx=1;
+             while(j<store.size()){
+                temp[idx]=store[i]+store[j];
+                i++;
+                j++;
+                idx++;
+             }
+             res.push_back(temp);
+        }
+        return res;
     }
 };
