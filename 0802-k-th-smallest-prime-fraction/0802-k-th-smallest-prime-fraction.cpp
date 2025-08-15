78376fb081d15833b28297d28505d6ef9ab3898e
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-15 21:11:35 +0530

    Time: 191 ms (79.14%), Space: 12.4 MB (76.84%) - LeetHub

--- 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.cpp
+++ 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.cpp
@@ -2,21 +2,24 @@ class Solution {
     typedef pair<double,pair<int,int>> P;
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        int n=arr.size();
         priority_queue<P,vector<P>,greater<P>> min_heap;
-        int iterations=k;
-        int curr_last=arr.size()-1;
-        while(iterations>0){
-           for(int i=0;i<curr_last;i++){
-              double fraction=1.0*arr[i]/arr[curr_last];
-              min_heap.push({fraction,{arr[i],arr[curr_last]}});
-           }
-           iterations--;
-           if(iterations==0) return {min_heap.top().second.first,min_heap.top().second.second};
-
-           min_heap.pop();
-           curr_last--;
+        for(int i=0;i<n-1;i++){
+            double fraction=1.0*arr[i]/arr[n-1];
+            min_heap.push({fraction,{i,n-1}});
         }
-      return {};
-    
+
+        while(k>1){
+           P temp=min_heap.top();
+           min_heap.pop();
+           if(temp.second.second-1>temp.second.first){
+           double fraction=1.0*arr[temp.second.first]/arr[temp.second.second-1];
+           min_heap.push({fraction,{temp.second.first,temp.second.second-1}});
+           }
+                k--;
+        }
+   int i=min_heap.top().second.first;
+   int j=min_heap.top().second.second;
+   return  {arr[i],arr[j]};
     }
 };
