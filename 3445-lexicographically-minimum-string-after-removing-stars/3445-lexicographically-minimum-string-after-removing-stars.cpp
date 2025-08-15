b2ff3feb7b1dfd68b1e77777e2841aecaa78a557
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-15 23:00:14 +0530

    Time: 65 ms (77.31%), Space: 24.4 MB (89.12%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,25 +1,27 @@
 class Solution {
-    static bool comp(pair<char,int> p1,pair<char,int> p2){
-        if(p1.first==p2.first) return p1.second<p2.second;
-
-        return p1.first>p2.first;
-    }
 public:
     string clearStars(string s) {
-        priority_queue<pair<char,int>,vector<pair<char,int>>,decltype(&comp)> min_heap(comp);
-        vector<bool> vis(s.size());
+        vector<vector<int>> mp(26);
         int n=s.size();
+        vector<bool> vis(n);
         for(int i=0;i<n;i++){
             if(s[i]=='*'){
-                vis[min_heap.top().second]=1;
-                min_heap.pop();
-                continue;
+              for(int i=0;i<26;i++){
+                if(mp[i].size()>0){
+                    vis[mp[i].back()]=1;
+                    mp[i].pop_back();
+                    break;
+                }
+              }
+              continue;
             }
-            min_heap.push({s[i],i});
+            mp[s[i]-'a'].push_back(i);
         }
         string res="";
-        for(int i=0;i<s.size();i++){
-            if(!vis[i] && s[i]!='*') res+=s[i];
+        for(int i=0;i<n;i++){
+            if(!vis[i] && s[i]!='*'){
+                res+=s[i];
+            }
         }
         return res;
     }
