b585202434bb32d622a0a93ab191d5875eb07455
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-17 23:50:12 +0530

    Time: 63 ms (86.25%), Space: 11.3 MB (86.11%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,20 +1,34 @@
 class Solution {
+    typedef pair<int,int> p;
     const int MOD=1e9+7;
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<long long> vec;
+        priority_queue<p,vector<p>,greater<p>> min_heap;
+      
         for(int i=0;i<n;i++){
-            long long sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                vec.push_back(sum);
+           min_heap.push({nums[i],i});
+        }
+
+        int count=0;
+        int left_half_sum=0;
+      int total_sum=0;
+        while(count<=right){
+            count++;
+            p store=min_heap.top();
+            min_heap.pop();
+            if(count<left){
+                left_half_sum=(left_half_sum+store.first)%MOD;
+                total_sum=(total_sum+store.first)%MOD;
+            }
+            else if(count<=right){
+                total_sum=(total_sum+store.first)%MOD;
+            }
+            int idx=store.second;
+            idx+=1;
+            if(idx<n){
+                min_heap.push({(store.first+nums[idx])%MOD,idx});
             }
         }
-        sort(vec.begin(),vec.end());
-        long long res=0;
-        for(int i=left-1;i<right;i++){
-            res=(res+vec[i])%MOD;
-        }
-        return res;
+      return (total_sum-left_half_sum);
     }
 };
