d2eaf08afb58dec6b82a11ce79e2934266433c3a
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-20 00:11:13 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -8,9 +8,7 @@ public:
         freq1[s[i]-'a']++;
         freq2[t[i]-'a']++;
       }
-      for(int i=0;i<26;i++){
-        if(freq1[i]!=freq2[i]) return false;
-      }
-      return true;
+    
+      return freq1==freq2;
     }
 };
