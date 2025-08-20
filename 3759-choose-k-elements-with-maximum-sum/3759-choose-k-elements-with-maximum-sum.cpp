f8d60a691955dfff3fc177cb2575ec28c6b95a2c
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-20 21:40:09 +0530

    Time: 87 ms (88.54%), Space: 178.9 MB (80.9%) - LeetHub

--- 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.cpp
+++ 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.cpp
@@ -1,5 +1,11 @@
 class Solution {
     typedef pair<int,int> P;
+    void fun1(priority_queue<int,vector<int>,greater<int>> &heap,long long &sum,vector<pair<int,int>> &arr,vector<int>&nums2,int idx){
+         sum+=nums2[arr[idx].second];
+                heap.push(nums2[arr[idx].second]);
+
+    }
+
 public:
     vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
         int n=nums1.size();
@@ -20,28 +26,25 @@ public:
             if(i>0 && arr[i-1].first==arr[i].first){
                 ans[arr[i].second]=ans[arr[i-1].second];
                if(heap.size()<=k){
-                  sum+=nums2[arr[i].second];
-                heap.push(nums2[arr[i].second]);
+                 fun1(heap,sum,arr,nums2,i);
                }
                else{
-                  sum-=heap.top();
+                     sum-=heap.top();
                 heap.pop();
-                sum+=nums2[arr[i].second];
-                heap.push(nums2[arr[i].second]);
+                fun1(heap,sum,arr,nums2,i);
                }
                 continue;
             }
             if(heap.size()<=k){
                 ans[arr[i].second]=sum;
-                sum+=nums2[arr[i].second];
-                heap.push(nums2[arr[i].second]);
+                  fun1(heap,sum,arr,nums2,i);
             }
             else if(heap.size()>k){
                 sum-=heap.top();
                 heap.pop();
                 ans[arr[i].second]=sum;
-                sum+=nums2[arr[i].second];
-                heap.push(nums2[arr[i].second]);
+                   fun1(heap,sum,arr,nums2,i);
+           
             }
         }
         return ans;
