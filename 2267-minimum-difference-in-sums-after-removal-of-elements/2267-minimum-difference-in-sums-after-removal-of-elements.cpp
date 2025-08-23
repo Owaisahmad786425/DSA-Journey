f7db5198d142862675d457ac69a4e6961637ed01
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-23 12:16:51 +0530

    Time: 132 ms (47.9%), Space: 143.9 MB (66.57%) - LeetHub

--- 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     long long minimumDifference(vector<int>& nums) {
-        long long N=nums.size();
-        priority_queue<long long> max_heap;
-        priority_queue<long long,vector<long long>,greater<long long>> min_heap;
-        vector<long long> left_min(N,0),right_max(N,0);
-        long long n=N/3;
-        long long sum1=0;
-        long long  sum2=0;
-        //for left store it owais
-        for(int i=0;i<N-n;i++){
-            sum1+=nums[i];
+        int size=nums.size();
+        int n=size/3;
+        vector<long long> prefix(size,0);
+        long long sum=0;
+        priority_queue<int,vector<int>,greater<int>> min_heap;
+        priority_queue<int> max_heap;
+        for(int i=0;i<2*n;i++){
+            sum+=nums[i];
             max_heap.push(nums[i]);
+
             if(max_heap.size()>n){
-                sum1-=max_heap.top();
-                max_heap.pop();
+               sum-=max_heap.top();
+               max_heap.pop();
             }
-            left_min[i]=sum1;
+            prefix[i]=sum;
         }
-        //for right store it owais
-        for(int i=N-1;i>=n;i--){
-            sum2+=nums[i];
+        long long res=LLONG_MAX;
+        sum=0;
+        for(int i=size-1;i>=n;i--){
+            sum+=nums[i];
             min_heap.push(nums[i]);
-            if(min_heap.size()>n){
-                sum2-=min_heap.top();
+
+            if(min_heap.size()==n){
+                res=min(res,(prefix[i-1]-sum));
+                sum-=min_heap.top();
                 min_heap.pop();
             }
-            right_max[i]=sum2;
         }
-        long long res=LLONG_MAX;  
-
-        for(int i=n-1;i<N-n;i++){
-            res=min(res,(left_min[i]-right_max[i+1]));
-        }
-        return res;
-
+return  res;
     }
 };
