325db6324af8a8b465a5dac120271e76806ff8a2
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-29 22:28:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (0%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
- return (long long)(((1LL*m)*(1LL*n))/2);
+    
+     long long a=1LL*((n+1)/2)*(m/2);
+     long long b=1LL*((m+1)/2)*(n/2);
+     return a+b;
+
+
+
     }
 };
