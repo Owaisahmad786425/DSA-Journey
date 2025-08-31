787571c5ad7fbb9349447208d089f7f315e61898
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-08-31 21:23:52 +0530

    Time: 0 ms (100%), Space: 9.9 MB (58.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,38 +1,16 @@
 class Solution {
-    bool check(char a){
-        if((a>=65 && a<=90)|| (a>=97 && a<=122)) return true;
-
-        return false;
-    }
-    bool check2(char a){
-        if(a>='0' && a<='9') return true;
-
-        return false;
-    }
 public:
     bool isPalindrome(string s) {
         int low=0;
         int high=s.size()-1;
         while(low<=high){
-            if(!isalnum(s[low]) && !isalnum(s[high])){
-                high--;
-                low++;
-            }
-            else if(!isalnum(s[low])) low++;
-            else if(!isalnum(s[high])) high--;
-            else if(check(s[low])&& check(s[high])){
-                if(tolower(s[low])!=tolower(s[high])) return false;
-                low++;
-                high--;
-            }
-            else if(check2(s[low])&& check2(s[high])){
-                if(s[low]!=s[high])return false;
-                low++;
-                high--;
-            }
-            else if((check2(s[low])&& check(s[high])) ||(check(s[low])&& check2(s[high]))) return false;
-        }
+            while(low<=high && !isalnum(s[low])) low++;
+            while(low<=high && !isalnum(s[high])) high--;
+            if( low<=high && tolower(s[low])!=tolower(s[high])) return false;
 
+            low++;
+            high--;
+        }
         return true;
     }
 };
