3e138c32dad8b933555b17eaf7b5f8dd6f6816af
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-09-03 02:08:47 +0530

    Time: 47 ms (15.22%), Space: 41.7 MB (15.94%) - LeetHub

---
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    static bool comp(vector<int> vec1,vector<int> vec2){
+        if(vec1[0]==vec2[0]){
+            return vec1[1]>vec2[1];
+        }
+        return vec1[0]<vec2[0];
+    }
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(),points.end(),comp);
+        int res=0;
+        for(int i=0;i<points.size();i++){
+            for(int j=0;j<points.size();j++){
+              if(i==j) continue;
+                int x1=points[i][0];
+                int y1=points[i][1];
+                int x2=points[j][0];
+                int y2=points[j][1];
+       
+                if(x1>=x2 && y1<=y2){
+                    bool flag=0;
+                    for(int k=0;k<points.size();k++){
+                        if(i==k || k==j) continue;
+                        int x3=points[k][0];
+                        int y3=points[k][1];
+                        if(x3<=x1 && x3>=x2 && y3>=y1 && y3<=y2){
+                            flag=true;
+                            break;
+                        }
+
+                }
+                if(!flag) res++;
+                
+            }
+        }
+       
+    }
+    return res;
+    }
+};
