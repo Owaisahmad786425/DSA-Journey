777fd02204845d48c2d0d868929d958d1562c0e1
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-09-06 04:31:15 +0530

    Time: 63 ms (19.4%), Space: 153.3 MB (12.58%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    typedef pair<int,int> P;
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+     priority_queue<P> max_heap;
+     for(int i=0;i<k;i++){
+        max_heap.push({nums[i],i});
+     }   
+     vector<int> res;
+     res.push_back(max_heap.top().first);
+     for(int i=k;i<nums.size();i++){
+       int idx=max_heap.top().second;
+       idx=(idx+k-1);
+       if(i<=idx){
+        max_heap.push({nums[i],i});
+        res.push_back(max_heap.top().first);
+       }
+       else{
+        while(!max_heap.empty() && idx<i){
+            max_heap.pop();
+          idx=max_heap.top().second;
+             idx=(idx+k-1);
+        }
+         max_heap.push({nums[i],i});
+        res.push_back(max_heap.top().first);
+       }
+     }
+
+return res;
+    }
+};
