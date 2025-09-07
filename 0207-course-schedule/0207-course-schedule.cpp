9872e1e7261a9f3f60207ddf5a4a34e964bacb7a
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-09-08 00:13:21 +0530

    Time: 0 ms (100%), Space: 19.3 MB (74.94%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    bool dfs(int node,vector<vector<int>> &adj,vector<bool> &visited,vector<bool> &path_visited){
-        visited[node]=true;
-        path_visited[node]=true;
-        for(auto it:adj[node]){
-            if(path_visited[it]) return false;
-            if(!visited[it]){
-            if(!dfs(it,adj,visited,path_visited)) return false;
-            }
-        }
-       path_visited[node]=false;
-        return true;
-    }
+    // bool dfs(int node,vector<vector<int>> &adj,vector<bool> &visited,vector<bool> &path_visited){
+    //     visited[node]=true;
+    //     path_visited[node]=true;
+    //     for(auto it:adj[node]){
+    //         if(path_visited[it]) return false;
+    //         if(!visited[it]){
+    //         if(!dfs(it,adj,visited,path_visited)) return false;
+    //         }
+    //     }
+    //    path_visited[node]=false;
+    //     return true;
+    // }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
        vector<vector<int>> adj(numCourses);
@@ -19,13 +19,31 @@ public:
         int v=prerequisites[i][0];
        adj[u].push_back(v);
        } 
-       vector<bool> visited(numCourses,0);
-       vector<bool> path_visited(numCourses,0);
-       for(int i=0;i<numCourses;i++){
-        if(!visited[i]){
-            if(!dfs(i,adj,visited,path_visited)) return false;
+    vector<int> in_degree(numCourses,0);
+    for(int i=0;i<adj.size();i++){
+        for(auto it:adj[i]){
+          in_degree[it]++;
         }
-       }
-       return true;
+    }
+  queue<int> q;
+      int count=0;
+    for(int i=0;i<in_degree.size();i++){
+        if(in_degree[i]==0){
+            q.push(i);
+        }
+    }
+
+  while(!q.empty()){
+    int node=q.front();
+    count++;
+    q.pop();
+    for(auto it:adj[node]){
+      in_degree[it]--;
+      if(in_degree[it]==0){
+        q.push(it);
+      }
+    }
+  }
+  return count==numCourses;
     }
 };
