d3a95d2bcab6c747c5a2f0aa0b01aa86bb5be127
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-09-11 01:20:03 +0530

    Time: 231 ms (52.94%), Space: 152.7 MB (17.65%) - LeetHub

--- 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
-     int users=languages.size();
-     int relations=friendships.size();
-     int res=1e9;
-     for(int i=1;i<=n;i++){
-        int count=0;
-        for(int j=0;j<relations;j++){
-            int u=friendships[j][0];
-            int v=friendships[j][1];
+        unordered_map<int,int> freq;
+        int m=languages.size();
+        vector<bool> vis(m,0);
+        vector<int> sad_users;
+        for(int i=0;i<friendships.size();i++){
+            unordered_set<int> st;
+            int u=friendships[i][0];
+            int v=friendships[i][1];
+            for(int j=0;j<languages[u-1].size();j++){
+                st.insert(languages[u-1][j]);
+            }
             bool flag=0;
-            for(int k=0;k<languages[u-1].size();k++){
-                if(i==languages[u-1][k]) {
+            for(int j=0;j<languages[v-1].size();j++){
+                if(st.find(languages[v-1][j])!=st.end()){
                     flag=1;
                     break;
                 }
             }
             if(!flag){
-             count++;
-                languages[u-1].push_back(i);
-            }
-            flag=0;
-             for(int k=0;k<languages[v-1].size();k++){
-                if(i==languages[v-1][k]) {
-                    flag=1;
-                    break;
+                if(!vis[u-1]){
+                vis[u-1]=1;
+                sad_users.push_back(u-1);
+                }
+                if(!vis[v-1]){
+                    vis[v-1]=1;
+                sad_users.push_back(v-1);
                 }
             }
-             if(!flag){
-                count++;
-                languages[v-1].push_back(i);
-             }
         }
-        res=min(res,count);
-     }
-
-return res;
+        for(int i=0;i<sad_users.size();i++){
+           for(int j=0;j<languages[sad_users[i]].size();j++){
+            freq[languages[sad_users[i]][j]]++;
+           }
+        }
+        int maxi=0;
+        for(auto it:freq){
+            maxi=max(maxi,it.second);
+        }
+        return sad_users.size()-maxi;
     }
 };
