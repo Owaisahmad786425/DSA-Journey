b3257ac3703ac382aea40900890c1b5de7af535f
Owaisahmad786425 <rizwanaperween935@gmail.com>
2025-09-22 01:36:34 +0530

    Time: 3 ms (48.81%), Space: 8.8 MB (76.73%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -2,22 +2,37 @@ class Solution {
 public:
     string reverseParentheses(string s) {
         stack<int> st;
+        unordered_map<int,int> mp;
         int n=s.size();
         for(int i=0;i<n;i++){
-            if(s[i]!=')' &&s[i]!='(') continue;
+            if(s[i]!='(' && s[i]!=')') continue;
 
             if(s[i]=='('){
                 st.push(i);
             }
             else if(s[i]==')'){
-                reverse(s.begin()+st.top()+1,s.begin()+i);
-                st.pop();
+               mp[st.top()]=i;
+               mp[i]=st.top();
+               st.pop();
             }
         }
+
         string res="";
-        for(auto ch:s){
-            if(ch==')' || ch=='(') continue;
-            res+=ch;
+        int i=0;
+        bool flag=0;
+        while(i<n){
+            if(s[i]=='(' || s[i]==')'){
+                i=mp[i];
+                flag=!flag;
+            }
+            else{
+                res+=s[i];
+            }
+
+            if(!flag) i++;
+            else{
+                i--;
+            }
         }
         return res;
     }
